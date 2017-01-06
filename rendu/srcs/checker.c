be7b73c932b76c6c83cc3b2186bf81de6b5fa9ae
Benjamin Duron (linux) <bduron@student.42.fr>
2017-01-06 08:38:04 +0100

    0.8 error handling - duplicates ok

--- rendu/srcs/checker.c
+++ rendu/srcs/checker.c
@@ -12,6 +12,23 @@
 
 #include "push_swap.h"
 
+int error_duplicate(int argc, char **argv)
+{
+	int i;
+	int j;
+	
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;		 
+		while (j < argc)
+			if (ft_atoi(argv[i]) == ft_atoi(argv[j++]))
+				return (1); 
+		i++;
+	}	
+	return (0);
+}
+
 int error_arg(int argc, char **argv)
 {
 	int i;
@@ -24,11 +41,8 @@ int error_arg(int argc, char **argv)
 		if (argv[i][j] == '-')
 			j++;
 		while (argv[i][j])
-		{
-			if (ft_isdigit(argv[i][j]) == 0)
+			if (ft_isdigit(argv[i][j++]) == 0)
 				return (1); 
-			j++;
-		} 	
 		if (j - (argv[i][0] == '-') > 10)
 			return (1); 
 		if (j - (argv[i][0] == '-') == 10)
@@ -37,6 +51,8 @@ int error_arg(int argc, char **argv)
 				return (1); 
 		i++;
 	}
+	if (error_duplicate(argc, argv))
+		return (1);
 	return (0); 
 }	
 
@@ -45,7 +61,7 @@ int main(int argc, char **argv)
 	if (error_arg(argc, argv))
 		ft_putstr("Error\n");
 	else 
-		ft_putstr("Ok\n");
+		ft_putstr("Ok\n"); // TO DEL
 	
 
 	return (0);
